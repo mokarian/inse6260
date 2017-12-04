a28f55cc0fdd7bba412dd5176f0d0e5b545dbec9
freyjaj93 <fjkulsdt@unb.ca>
2017-12-03 22:08:53 -0500

    Three more unit tests finished.
    4 unit tests left.

--- src/test/java/com/model/sc/ScheduleTest.java
+++ src/test/java/com/model/sc/ScheduleTest.java
@@ -1,8 +1,11 @@
 package com.model.sc;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
@@ -14,20 +17,36 @@ import static org.junit.Assert.*;
  * @since 03.12.2017
  */
 public class ScheduleTest {
+    private Schedule schedule;
+
     @Before
     public void setUp() throws Exception {
+        this.schedule = new Schedule();
     }
 
     @After
     public void tearDown() throws Exception {
+        this.schedule = null;
     }
 
     @Test
     public void getTimeLine() throws Exception {
+        this.schedule.setTimeLine(Arrays.stream(TimeLine.values()).filter(e -> e.name().equalsIgnoreCase(
+                "MONDAY_9AM_TO_12PM")).findAny().orElse(null));
+
+        Assert.assertNotNull(this.schedule.getTimeLine());
+        Assert.assertEquals(Arrays.stream(TimeLine.values()).filter(e -> e.name().equalsIgnoreCase(
+                "MONDAY_9AM_TO_12PM")).findAny().orElse(null), this.schedule.getTimeLine());
     }
 
     @Test
     public void getCourseType() throws Exception {
+        this.schedule.setCourseType(Arrays.stream(CourseType.values()).filter(e -> e.name().equalsIgnoreCase(
+                "LECTURE")).findAny().orElse(null));
+
+        Assert.assertNotNull(this.schedule.getCourseType());
+        Assert.assertEquals(Arrays.stream(CourseType.values()).filter(e -> e.name().equalsIgnoreCase(
+                "LECTURE")).findAny().orElse(null), this.schedule.getCourseType());
     }
 
 }
--- src/test/java/com/model/sc/StudentTest.java
+++ src/test/java/com/model/sc/StudentTest.java
@@ -1,8 +1,13 @@
 package com.model.sc;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.Assert.*;
 
@@ -14,20 +19,39 @@ import static org.junit.Assert.*;
  * @since 03.12.2017
  */
 public class StudentTest {
+    private Student student;
+
     @Before
     public void setUp() throws Exception {
+        this.student = new Student();
     }
 
     @After
     public void tearDown() throws Exception {
+        this.student = null;
     }
 
     @Test
     public void getAddress() throws Exception {
+        String expectedAddress = "91 University Avenue, Montreal, H3K 1Z2, Quebec";
+        this.student.setAddress(expectedAddress);
+
+        Assert.assertEquals(expectedAddress, this.student.getAddress());
     }
 
     @Test
     public void getCourseHistory() throws Exception {
+        Course course1 = new Course();
+        course1.setCourseName("INSE 6261");
+        Course course2 = new Course();
+        course2.setCourseName("COMP 6651");
+        Set<Course> expectedCourseHistory = new HashSet<>();
+        expectedCourseHistory.add(course1);
+        expectedCourseHistory.add(course2);
+
+        this.student.setCourseHistory(expectedCourseHistory);
+
+        Assert.assertEquals(expectedCourseHistory, this.student.getCourseHistory());
     }
 
     @Test
@@ -36,14 +60,17 @@ public class StudentTest {
 
     @Test
     public void getTuition() throws Exception {
+        BigDecimal expectedTuition = new BigDecimal(7500.00);
+        this.student.setTuition(expectedTuition);
+
+        Assert.assertEquals(expectedTuition, this.student.getTuition());
     }
 
     @Test
     public void getPhone() throws Exception {
-    }
+        String expectedPhone = "(514)888-0000";
+        this.student.setPhone(expectedPhone);
 
-    @Test
-    public void toStringTest() throws Exception {
+        Assert.assertEquals(expectedPhone, this.student.getPhone());
     }
-
 }
